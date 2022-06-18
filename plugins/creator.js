23154e10b7fca4e402abac8a69309ec4c5a33d94
Putrabotz02 <107519792+Putrabotz02@users.noreply.github.com>
2022-06-18 22:04:21 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '628818770766', owname, m)
-  conn.sendContact(m.chat, '628818770766', owname, m)
+  // this.sendContact(m.chat, '6283840600765', owname, m)
+  conn.sendContact(m.chat, '6283840600765', owname, m)
   conn.reply(m.chat, `Tuh Ownerku hehe`, m)
 }
 handler.help = ['owner']
